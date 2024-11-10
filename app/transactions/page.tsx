165518323de7c719b45052d5d9da89e9a444c03b
Matheus Kamer <matheus_kamer@hotmail.com>
2024-11-10 13:44:42 -0400

    feat: implement Prisma client for database access in transactions page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,3 +1,16 @@
-export default function Transactions() {
-  return <h1>Transactions page</h1>
+import { db } from "../_lib/prisma";
+
+export default async function Transactions() {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div>
+      <h1>Transactions</h1>
+      <ul>
+        {transactions.map((transaction) => (
+          <li key={transaction.id}>{transaction.name}</li>
+        ))}
+        ;
+      </ul>
+    </div>
+  );
 }
