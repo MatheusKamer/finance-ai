aa83b1b4b5778f203a5dfc107029a83c418c25b2
Matheus Kamer <matheus_kamer@hotmail.com>
2024-11-12 21:34:07 -0400

    feat: add data table component and transaction columns for improved transaction display

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,16 +1,21 @@
 import { db } from "../_lib/prisma";
+import { Button } from "../_components/ui/button";
+import { ArrowDownUpIcon } from "lucide-react";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
 
 export default async function Transactions() {
   const transactions = await db.transaction.findMany({});
   return (
-    <div>
-      <h1>Transactions</h1>
-      <ul>
-        {transactions.map((transaction) => (
-          <li key={transaction.id}>{transaction.name}</li>
-        ))}
-        ;
-      </ul>
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar Transação
+          <ArrowDownUpIcon size={16} />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
     </div>
   );
 }
