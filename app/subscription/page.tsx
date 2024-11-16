b535b83da80107ca2a6fcf6a35636ba25a9b255f
Matheus Kamer <matheus_kamer@hotmail.com>
2024-11-16 15:29:40 -0400

    feat: add authentication check to Subscription and Transactions pages with redirect to login

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,13 @@
+import { redirect } from "next/navigation";
 import { Navbar } from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
 
 export default function Subscription() {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
   return <Navbar />;
 }
