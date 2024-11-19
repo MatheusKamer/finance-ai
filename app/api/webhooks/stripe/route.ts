3ab211ab0a1c8906c70470d8cd79001a11be0c46
Matheus Kamer <matheus_kamer@hotmail.com>
2024-11-18 20:33:52 -0400

    feat: update Stripe webhook handling and improve error responses

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -2,27 +2,18 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-export async function POST(request: Request) {
+export const POST = async (request: Request) => {
+  if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
+    return NextResponse.error();
+  }
   const signature = request.headers.get("stripe-signature");
-
   if (!signature) {
-    return new Response("No signature", { status: 400 });
+    return NextResponse.error();
   }
-
   const text = await request.text();
-
-  if (!process.env.STRIPE_SECRET_KEY) {
-    return new Response("No secret key", { status: 500 });
-  }
-
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2024-10-28.acacia",
   });
-
-  if (!process.env.STRIPE_WEBHOOK_SECRET) {
-    return new Response("No webhook secret", { status: 500 });
-  }
-
   const event = stripe.webhooks.constructEvent(
     text,
     signature,
@@ -31,12 +22,12 @@ export async function POST(request: Request) {
 
   switch (event.type) {
     case "invoice.paid": {
+      // Atualizar o usuário com o seu novo plano
       const { customer, subscription, subscription_details } =
         event.data.object;
-
       const clerkUserId = subscription_details?.metadata?.clerk_user_id;
       if (!clerkUserId) {
-        return new Response("No clerk user id", { status: 400 });
+        return NextResponse.error();
       }
       await clerkClient().users.updateUser(clerkUserId, {
         privateMetadata: {
@@ -50,13 +41,13 @@ export async function POST(request: Request) {
       break;
     }
     case "customer.subscription.deleted": {
+      // Remover plano premium do usuário
       const subscription = await stripe.subscriptions.retrieve(
         event.data.object.id,
       );
-
       const clerkUserId = subscription.metadata.clerk_user_id;
       if (!clerkUserId) {
-        return new Response("No clerk user id", { status: 400 });
+        return NextResponse.error();
       }
       await clerkClient().users.updateUser(clerkUserId, {
         privateMetadata: {
@@ -67,8 +58,7 @@ export async function POST(request: Request) {
           subscriptionPlan: null,
         },
       });
-      break;
     }
   }
   return NextResponse.json({ received: true });
-}
+};
